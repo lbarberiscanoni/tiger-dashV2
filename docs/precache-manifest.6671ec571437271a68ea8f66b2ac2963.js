3e301b7bc849f348e0aa32fcd521bd80f80f7728
Lorenzo Barberis Canonico <lbarberiscanoni@Lorenzos-MacBook-Pro.local>
2018-10-31 14:57:31 -0400

    trying out at the docs thing

---
+++ docs/precache-manifest.6671ec571437271a68ea8f66b2ac2963.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "f79ac305b61161e796c6",
+    "url": "/static/css/main.eb7703cf.chunk.css"
+  },
+  {
+    "revision": "f79ac305b61161e796c6",
+    "url": "/static/js/main.f79ac305.chunk.js"
+  },
+  {
+    "revision": "b5beef6d4d353bbb73fd",
+    "url": "/static/js/1.b5beef6d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "16ad975f98719ee601ac20a91ab222b2",
+    "url": "/index.html"
+  }
+];
