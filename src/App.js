1b12381619e76bc00dfdb037cf373ac8e1d22141
Lorenzo Barberis Canonico <lbarberiscanoni@Lorenzos-MacBook-Pro.local>
2018-11-12 14:47:20 -0500

    finally got a basic vis done

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 import TestChart from "./TestChart";
 
-const Index = () => <div><h2>Home</h2><TestChart /></div>
+const Index = () => <div><TestChart /></div>
 const Map = () => <h2>Map</h2>;
 const Saved = () => <h2>Saved</h2>;
 const Upload = () => <h2>Upload</h2>;
@@ -13,35 +13,33 @@ const App = () => (
       <h5 className="brand-logo center">Tiger Dash</h5>
     </div>
     <Router>
-      <div className="row">
-        <div className="col s3 m4 l3">
+      <div className="row" style={{ display: "flex" }}>
+        <div className="col s1 m2 l1" style={{ width: "20%" }}>
           <div className="nav-wrapper">
             <nav style={{ height: 250 }}>
-              <ul>
+              <ul style={{ padding: 0 }}>
                 <li style={{ float: "none" }}>
-                  <Link to="/">Home</Link>
+                  <Link to="/" className="center-align">Home</Link>
                 </li>
                 <li style={{ float: "none" }}>
-                  <Link to="/map/">Map</Link>
+                  <Link to="/map/" className="center-align">Map</Link>
                 </li>
                 <li style={{ float: "none" }}>
-                  <Link to="/saved/">Saved</Link>
+                  <Link to="/saved/" className="center-align">Saved</Link>
                 </li>
                 <li style={{ float: "none" }}>
-                  <Link to="/upload/">Upload</Link>
+                  <Link to="/upload/" className="center-align">Upload</Link>
                 </li>
               </ul>
             </nav>
           </div>
         </div>
 
-        <div className="col s9 m8 l9">
-          <div className="container">
+        <div className="col s11 m10 l11" style={{ flex: 1 }}>
             <Route path="/" exact component={Index} />
             <Route path="/map/" component={Map} />
             <Route path="/saved/" component={Saved} />
             <Route path="/upload/" component={Upload} />
-          </div>
         </div>
       </div>
     </Router>
--- src/TestChart.js
+++ src/TestChart.js
@@ -1,6 +1,38 @@
 import React, { Component } from 'react';
 import {Line as LineChart, Bar as BarChart} from 'react-chartjs';
 
+class ChartContainer extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+
+	render() {
+		return (
+			<div className="col s6 m6 l6">
+				<div className="col s12 m12 l12">
+					<form>
+						{ this.props.children }
+					</form>
+				</div>
+				<div className="col s12 m12 l12">
+					{ this.props.chart }
+				</div>
+				<div className="row">
+					<div className="col s4 m4 l4"></div>
+					<div className="col s4 m4 l4">
+						<button className="btn waves-effect waves-light"> 
+							SAVE
+							<i className="material-icons right">save</i>
+						</button>
+					</div>
+					<div className="col s4 m4 l4"></div>
+				</div>
+			</div>
+		)
+	}
+}
+
 class TestChart extends Component {
 
 	constructor(props) {
@@ -24,16 +56,31 @@ class TestChart extends Component {
 	render() {
 
 		return(
-			<div>
-				<form>
+			<div className="row">
+				<ChartContainer chart={ <LineChart data={this.state} options={null} height="300px" width="500px" /> }>
 					{
 						this.state.labels.map(x => {
-							return <label><input type="checkbox" /><span>{ x }</span></label>
+							return <div>
+								<label>
+									<input type="checkbox" />
+									<span>{ x }</span>
+								</label>
+							</div>
 						})
 					}
-				</form>
-				<LineChart data={this.state} options={null} width="600" height="250"/>
-				<BarChart data={this.state} options={null} width="600" height="250"/>
+				</ChartContainer>
+				<ChartContainer chart={ <BarChart data={this.state} options={null} height="300px" width="500px" /> }>
+					{
+						this.state.labels.map(x => {
+							return <div>
+								<label>
+									<input type="checkbox" />
+									<span>{ x }</span>
+								</label>
+							</div>
+						})
+					}
+				</ChartContainer>
 			</div>
 		)
 	}
