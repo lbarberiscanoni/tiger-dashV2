1257fe83a9fc51a9daf097a1a640fdc671025566
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2018-11-27 16:11:10 -0500

    Updates

---
+++ precache-manifest.71c334a45e889d58eeaee3438af5ba47.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8025ce7b3a5866b1580",
+    "url": "/tiger-dashV2/static/js/main.c8025ce7.chunk.js"
+  },
+  {
+    "revision": "87f4061e4c6aad929f96",
+    "url": "/tiger-dashV2/static/js/1.87f4061e.chunk.js"
+  },
+  {
+    "revision": "c59ec4d8f6f62cb4d244",
+    "url": "/tiger-dashV2/static/js/runtime~main.c59ec4d8.js"
+  },
+  {
+    "revision": "c305e599a56d1af16f28733770afaec1",
+    "url": "/tiger-dashV2/index.html"
+  }
+];
