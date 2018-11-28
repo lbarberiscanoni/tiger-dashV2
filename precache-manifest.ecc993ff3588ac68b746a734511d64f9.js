eb02f9e74c934a35a65db352fe6eb539a9921a2e
lbarberiscanoni <lorenzo.barberis.canonico@gmail.com>
2018-11-27 22:23:45 -0500

    Updates

---
+++ precache-manifest.ecc993ff3588ac68b746a734511d64f9.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "466f16d1d0af5edf5697",
+    "url": "/tiger-dashV2/static/js/main.466f16d1.chunk.js"
+  },
+  {
+    "revision": "26f58db55ef89312b9ef",
+    "url": "/tiger-dashV2/static/js/1.26f58db5.chunk.js"
+  },
+  {
+    "revision": "c59ec4d8f6f62cb4d244",
+    "url": "/tiger-dashV2/static/js/runtime~main.c59ec4d8.js"
+  },
+  {
+    "revision": "e22f43f0400a135f874ec715ae2ce4c2",
+    "url": "/tiger-dashV2/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/tiger-dashV2/precache-manifest.647ac7cde7f68caf5163590ae1239516.js"
+  "/tiger-dashV2/precache-manifest.ecc993ff3588ac68b746a734511d64f9.js"
 );
 
 workbox.clientsClaim();
